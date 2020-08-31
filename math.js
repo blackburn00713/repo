a34b52edd3390c6b0f0ef69e333c9d37ecb7c6d8
blackburn00713 <roy.anibrata56@gmail.com>
2020-08-31 21:09:17 +0530

    maths functions added

---
+++ math.js
@@ -0,0 +1,10 @@
+var x=9;
+var y=3;
+console.log(Math.pow(9,3));
+console.log(Math.sqrt(9));
+console.log(Math.abs(-3.9))
+var z=Math.random();
+console.log(Math.round(z));
+console.log(Math.ceil(z));
+console.log(Math.floor(z));
+console.log(Math.sin(90 * Math.PI / 180))
