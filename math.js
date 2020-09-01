ac901362b9f335290e62c86de0d88b993dd195fe
blackburn00713 <roy.anibrata56@gmail.com>
2020-09-01 12:56:59 +0530

    fibonacci and number checker added

--- math.js
+++ math.js
@@ -1,10 +1,29 @@
-var x=9;
-var y=3;
-console.log(Math.pow(9,3));
-console.log(Math.sqrt(9));
-console.log(Math.abs(-3.9))
-var z=Math.random();
-console.log(Math.round(z));
-console.log(Math.ceil(z));
-console.log(Math.floor(z));
-console.log(Math.sin(90 * Math.PI / 180))
+function mynumber() {
+	var x=document.getElementById('num').value;
+	if((x<=100)&&(x>=10)){
+		document.getElementById('demo1').innerHTML="NUmber Is Between 10 to 100";
+	}
+	else{
+        document.getElementById('demo1').innerHTML="NUmber Is  not Between 10 to 100";
+	}
+}
+function fibn() {
+	var n=document.getElementById('num1').value;
+	var first=0;
+	var second=1;
+    var next;
+    var c;
+    for ( c = 0 ; c < n ; c+=1 )
+   {
+      if ( c <= 1 )
+         next = c;
+      else
+      {
+         next = first + second;
+         first = second;
+         second = next;
+      }
+      console.log(next);
+   }
+ document.getElementById('demo2').innerHTML="See The Answer in Console log";
+}
